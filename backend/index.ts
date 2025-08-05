17d3573e58afdc50be3b3445e5c298d373ad1df3
akash2061 <lucifermorningstar2061@gmail.com>
2025-08-05 10:52:02 +0530

    Allow 0.0.0.0

--- backend/index.ts
+++ backend/index.ts
@@ -23,7 +23,6 @@ app.use(errorHandler);
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(5000, '0.0.0.0', () => {
+    console.log('Server running on http://localhost:5000');
 });
